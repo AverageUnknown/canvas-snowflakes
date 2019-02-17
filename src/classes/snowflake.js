3adba8ff6738b8e69148b7a538fc2c067d9cdf3f
AverageUnknown <avgunknown@gmail.com>
2019-02-17 17:19:11 -0600

    correcting an issue where snowflakes could escape canvas bounds

--- src/classes/snowflake.js
+++ src/classes/snowflake.js
@@ -19,9 +19,9 @@ export default class Snowflake {
     draw(trace){
         this.calcPos();
         this.context.save()
-        this.context.scale(this.scale,this.scale);
         this.context.translate(this.x,this.y);
         this.context.rotate(this.r);
+        this.context.scale(this.scale,this.scale);
 
         trace();
 
@@ -29,17 +29,20 @@ export default class Snowflake {
     }
 
     calcPos(){
-            this.xv = Math.max(Math.min(Math.random()/2-.25 + this.xv,1),-1)
-            this.x = Math.max(Math.min(this.x + this.xv,this.canvas.width),0)
-            if (this.x == 0 || this.x == this.canvas.width){
-                this.xv = -this.xv;
-            }
-            this.yv = Math.max(Math.min(Math.random()/5-.1 + this.yv,1.25),.375)
-            this.y = this.y + this.yv
-            if (this.y > this.canvas.height){
-                this.y = 0;
-            }
-            this.rv = Math.max(Math.min((Math.random()*.0025-.00125)*Math.PI+this.rv,.025*Math.PI),-.025*Math.PI);
-            this.r = this.r + this.rv;
+        this.xv = Math.max(Math.min(Math.random()/2-.25 + this.xv,1),-1)
+        this.x = Math.max(Math.min(this.x + this.xv,this.canvas.width),0)
+        if (this.x > 1000){
+            console.log(this.x);
+        }
+        if (this.x == 0 || this.x == this.canvas.width){
+            this.xv = -this.xv;
+        }
+        this.yv = Math.max(Math.min(Math.random()/5-.1 + this.yv,1.25),.375)
+        this.y = this.y + this.yv
+        if (this.y > this.canvas.height){
+            this.y = 0;
+        }
+        this.rv = Math.max(Math.min((Math.random()*.0025-.00125)*Math.PI+this.rv,.025*Math.PI),-.025*Math.PI);
+        this.r = this.r + this.rv;
     }
 }
